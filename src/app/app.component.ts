f6877e771a5f1a9309356e7451c10cc15d56bc74
Subhan-Nasir <i40210@verisk.com>
2023-03-30 17:21:12 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'weather-app';
+
+  constructor(private weatherService: WeatherService){}
+
+  weatherData?: WeatherData;
+   
+  ngOnInit():void {
+    this.weatherService.getWeatherData("London").subscribe({
+      next: (response) => {
+        console.log(response);
+        this.weatherData = response;
+
+      }
+    });
+
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: "https://weatherapi-com.p.rapidapi.com/current.json",
+
+    XRapidAPIHostHeaderName: "X-RapidAPI-Host",
+    XRapidAPIHostHeaderValue: "weatherapi-com.p.rapidapi.com",
+
+    XRapidAPIKeyHeaderName: "X-RapidAPI-Key",
+    XRapidAPIKeyHeaderValue: "03a32b7d95msh0d54d727cede8c0p1a8f67jsn75dce36c1357"
+};
